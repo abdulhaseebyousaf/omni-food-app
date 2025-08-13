ede53895859f162c20b3e6745e542ea3ab8fc6e8
abdulhaseebyousaf <ahaseebyousaf@gmail.com>
2025-08-14 03:16:30 +0500

    Complete OmniFood app — fully responsive and functional on mobile & desktop

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,6 +1,6 @@
 import logo from '../assets/omnifood-logo.png'
-import FooterCards from '../common/FooterCards'
 import FooterAccount from '../common/FooterAccount'
+import FooterCards from './FooterCards'
 
 function Footer() {
   return (
@@ -8,7 +8,7 @@ function Footer() {
     <footer className='py-[128px] max-[1201px]:py-[115.5px] max-[945px]:py-[102.4px] border-t border-solid border-[#eee] max-sm:py-12'>
 
 <div className='grid gap-x-16 gap-y-24 max-sm:gap-y-5 max-[945px]:gap-x-[38.4px] max-[1201px]:max-w-[1080px] grid-cols-[1.5fr_1.5fr_1fr_1fr_1fr] max-w-[1200px] mx-auto px-8 max-[945px]:px-[25.6px] max-[705px]:grid-cols-6 max-[545px]:gap-y-[38px] max-[545px]:gap-x-[19px]'>
-<FooterCards 
+<FooterCards
 logo={logo}
 copyright={'Copyright © 2025 by Omnifood, Inc. All rights reserved.'}
 />
